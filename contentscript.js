6bf47fdb7f2d66182a495f08df7deb2300357a76
Andrea Piccinelli <andrea.piccinelli@imagovision.it>
2023-07-12 09:41:13 +0200

    Fixes #2: Updated the extension to adapt it to the current website.

--- contentscript.js
+++ contentscript.js
@@ -27,14 +27,33 @@ function downloadIcons8Svg(url, name){
       .catch(() => alert('Download failed, please try again.'));
 }
 
+function getSvgUrl(url) {
+    let parsedUrl = new URL(url);
+    parsedUrl.searchParams.set('format', 'svg');
+    return parsedUrl.toString();
+}
+
+function getSelectedIconName() {
+    let openCanvases = document.getElementsByClassName("app-accordion2__icon-preview");
+    if (openCanvases.length == 0) {
+        return "icon";
+    }
+    
+    let bigCanvas = openCanvases[0];
+    let link = bigCanvas.querySelector("a");
+    let url = link.getAttribute("href");
+    let iconName = url.substring(url.lastIndexOf("/") + 1); 
+    return iconName;
+}
+
 function tryToDownloadSelectedIcon() {
     let originalIconUrl = findSelectedIconUrl();
     if (originalIconUrl == null) {
         alert("Please select one icon before trying to download it.");
         return;
     }
-    let svgUrl = originalIconUrl.substring(0, originalIconUrl.lastIndexOf(".")) + ".svg";
-    let iconName = svgUrl.substring(originalIconUrl.lastIndexOf("/"));
+    let svgUrl = getSvgUrl(originalIconUrl);
+    let iconName = getSelectedIconName() + ".svg";
     downloadIcons8Svg(svgUrl, iconName);
 }
 
