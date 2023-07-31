95ecadb79c864f835f1e1911a3ab6b41049a88ae
Andrea Piccinelli <andrea.piccinelli@imagovision.it>
2023-07-31 09:37:28 +0200

    Now the extension works with both urls starting with /icon/ and /icons/

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
-chrome.action.onClicked.addListener(function (tab) { 
-  if (tab.url.indexOf("https://icons8.com/icon/") != -1) { 
+chrome.action.onClicked.addListener(function (tab) {
+    /* matches both /icon/ and /icons/ */
+  if (tab.url.indexOf("https://icons8.com/icon") != -1) { 
       chrome.scripting.executeScript({
         target: {tabId: tab.id},
         files: ['contentscript.js'],
