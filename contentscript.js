a9cb07c0eacae5645470383fa50c613e006be80c
Andrea Piccinelli <andrea.piccinelli@imagovision.it>
2023-09-04 16:54:32 +0200

    Some refactors from ChatGTP.
    *    Replaced getElementsByClassName with the more modern querySelectorAll.
    *    Used !openCanvases.length instead of openCanvases.length == 0 for clarity.
    *    Directly converted the byteCharacters string into an array of numbers using Array.from() and map().
    *    Removed the line iconDlElem.style.display = 'none'; because the a element will never be visible unless added to the DOM with a display block or similar.
    *    Removed delete iconDlElem; as it's not necessary. DOM elements are not deleted with delete. Using removeChild is sufficient.

--- contentscript.js
+++ contentscript.js
@@ -1,68 +1,60 @@
 function findSelectedIconUrl() {
-    let openCanvases = document.getElementsByClassName("app-accordion2__icon-preview");
-    if (openCanvases.length == 0) {
+    const openCanvases = document.querySelectorAll(".app-accordion2__icon-preview");
+    if (!openCanvases.length) {
         return null;
     }
 
-    let bigCanvas = openCanvases[0];
-    let icon = bigCanvas.querySelector("img");
-    let url = icon.getAttribute("srcset").split(" ")[0];
-    return url;
+    const bigCanvas = openCanvases[0];
+    const icon = bigCanvas.querySelector("img");
+    return icon.getAttribute("srcset").split(" ")[0];
 }
 
 function getSvgMetadataUrl(url) {
-    // FROM: https://img.icons8.com/?size=512&id=mHBlzWfNFiEi&format=png 
+    // FROM: https://img.icons8.com/?size=512&id=mHBlzWfNFiEi&format=png
     // TO:   https://api-icons.icons8.com/siteApi/icons/icon?id=mHBlzWfNFiEi&svg=true
-    let parsedUrl = new URL(url);
-    let iconId = parsedUrl.searchParams.get("id");
-    
-    let svgMetadataUrl = new URL("https://api-icons.icons8.com/siteApi/icons/icon?svg=true");
+    const parsedUrl = new URL(url);
+    const iconId = parsedUrl.searchParams.get("id");
+
+    const svgMetadataUrl = new URL("https://api-icons.icons8.com/siteApi/icons/icon?svg=true");
     svgMetadataUrl.searchParams.set("id", iconId);
     return svgMetadataUrl.toString();
 }
 
 function downloadSVGFromBase64(base64Data, fileName) {
     const byteCharacters = atob(base64Data);
-    const byteNumbers = new Array(byteCharacters.length);
-
-    for (let i = 0; i < byteCharacters.length; i++) {
-        byteNumbers[i] = byteCharacters.charCodeAt(i);
-    }
+    const byteNumbers = Array.from(byteCharacters).map(char => char.charCodeAt(0));
     const byteArray = new Uint8Array(byteNumbers);
     const blob = new Blob([byteArray], { type: "image/svg+xml" });
 
     const url = window.URL.createObjectURL(blob);
     const iconDlElem = document.createElement("a");
-    iconDlElem.style.display = 'none';
     iconDlElem.href = url;
     iconDlElem.download = fileName;
     document.body.appendChild(iconDlElem);
     iconDlElem.click();
+    document.body.removeChild(iconDlElem);
     window.URL.revokeObjectURL(url);
-    delete iconDlElem;
 }
 
 function downloadIcons8Svg(url) {
     fetch(url)
         .then(resp => resp.json())
         .then(json => {
-            console.log(json);
-            let svgData = json.icon.svg;
-            let iconName = json.icon.commonName;
-            let iconFileName = iconName + ".svg";
-          
+            const svgData = json.icon.svg;
+            const iconName = json.icon.commonName;
+            const iconFileName = `${iconName}.svg`;
             downloadSVGFromBase64(svgData, iconFileName);
-      })
-      .catch(() => alert('Download failed, please try again.'));
+        })
+        .catch(() => alert('Download failed, please try again.'));
 }
 
 function tryToDownloadSelectedIcon() {
-    let originalIconUrl = findSelectedIconUrl();
-    if (originalIconUrl == null) {
+    const originalIconUrl = findSelectedIconUrl();
+    if (!originalIconUrl) {
         alert("Please select one icon before trying to download it.");
         return;
     }
-    let svgMetadataUrl = getSvgMetadataUrl(originalIconUrl);
+    const svgMetadataUrl = getSvgMetadataUrl(originalIconUrl);
     downloadIcons8Svg(svgMetadataUrl);
 }
 
