605325f0899bb27b4e27160d3cac78c78add0f44
Andrea Piccinelli <andrea.piccinelli@imagovision.it>
2023-09-04 16:46:00 +0200

    Fixed the download with the current version. It's not as easy as before.
    You cannot simply change the file extension in the URL, we have to download the data as base64 and build the image in memory.

--- contentscript.js
+++ contentscript.js
@@ -10,40 +10,50 @@ function findSelectedIconUrl() {
     return url;
 }
 
-function downloadIcons8Svg(url, name){
+function getSvgMetadataUrl(url) {
+    // FROM: https://img.icons8.com/?size=512&id=mHBlzWfNFiEi&format=png 
+    // TO:   https://api-icons.icons8.com/siteApi/icons/icon?id=mHBlzWfNFiEi&svg=true
+    let parsedUrl = new URL(url);
+    let iconId = parsedUrl.searchParams.get("id");
+    
+    let svgMetadataUrl = new URL("https://api-icons.icons8.com/siteApi/icons/icon?svg=true");
+    svgMetadataUrl.searchParams.set("id", iconId);
+    return svgMetadataUrl.toString();
+}
+
+function downloadSVGFromBase64(base64Data, fileName) {
+    const byteCharacters = atob(base64Data);
+    const byteNumbers = new Array(byteCharacters.length);
+
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: "image/svg+xml" });
+
+    const url = window.URL.createObjectURL(blob);
+    const iconDlElem = document.createElement("a");
+    iconDlElem.style.display = 'none';
+    iconDlElem.href = url;
+    iconDlElem.download = fileName;
+    document.body.appendChild(iconDlElem);
+    iconDlElem.click();
+    window.URL.revokeObjectURL(url);
+    delete iconDlElem;
+}
+
+function downloadIcons8Svg(url) {
     fetch(url)
-      .then(resp => resp.blob())
-      .then(blob => {
-          const url = window.URL.createObjectURL(blob);
-          const iconDlElem = document.createElement('a');
-          iconDlElem.style.display = 'none';
-          iconDlElem.href = url;
-          iconDlElem.download = name;
-          document.body.appendChild(iconDlElem);
-          iconDlElem.click();
-          window.URL.revokeObjectURL(url);
-          delete iconDlElem;
+        .then(resp => resp.json())
+        .then(json => {
+            console.log(json);
+            let svgData = json.icon.svg;
+            let iconName = json.icon.commonName;
+            let iconFileName = iconName + ".svg";
+          
+            downloadSVGFromBase64(svgData, iconFileName);
       })
       .catch(() => alert('Download failed, please try again.'));
-}
-
-function getSvgUrl(url) {
-    let parsedUrl = new URL(url);
-    parsedUrl.searchParams.set('format', 'svg');
-    return parsedUrl.toString();
-}
-
-function getSelectedIconName() {
-    let openCanvases = document.getElementsByClassName("app-accordion2__icon-preview");
-    if (openCanvases.length == 0) {
-        return "icon";
-    }
-    
-    let bigCanvas = openCanvases[0];
-    let link = bigCanvas.querySelector("a");
-    let url = link.getAttribute("href");
-    let iconName = url.substring(url.lastIndexOf("/") + 1); 
-    return iconName;
 }
 
 function tryToDownloadSelectedIcon() {
@@ -52,9 +62,8 @@ function tryToDownloadSelectedIcon() {
         alert("Please select one icon before trying to download it.");
         return;
     }
-    let svgUrl = getSvgUrl(originalIconUrl);
-    let iconName = getSelectedIconName() + ".svg";
-    downloadIcons8Svg(svgUrl, iconName);
+    let svgMetadataUrl = getSvgMetadataUrl(originalIconUrl);
+    downloadIcons8Svg(svgMetadataUrl);
 }
 
 tryToDownloadSelectedIcon();
