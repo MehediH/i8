d4698d967d928c5e26a41870a28755b15f171dc9
Andrea Piccinelli <andrea.piccinelli@imagovision.it>
2023-09-13 10:09:52 +0200

    Add notification for disallowed domain on icons8.com
    - Added check for allowed domain
    - Show a notification if the domain is not allowed
    - Updated permissions in manifest.json to include "notifications"

--- background.js
+++ background.js
@@ -1,10 +1,18 @@
 chrome.action.onClicked.addListener(function (tab) {
-    /* matches both /icon/ and /icons/ */
-  if (tab.url.indexOf("https://icons8.com/icon") != -1) { 
-      chrome.scripting.executeScript({
+    const allowedDomain = "https://icons8.com";
+    if (!tab.url || tab.url.indexOf(allowedDomain) == -1) {
+        chrome.notifications.create({
+            type: 'basic',
+            iconUrl: 'icons/icon19.png',
+            title: 'Domain not allowed',
+            message: 'You can use this extension only on the icons8.com website.'
+        });
+        return;
+    }
+
+    chrome.scripting.executeScript({
         target: {tabId: tab.id},
         files: ['contentscript.js'],
     })
     .then(() => console.log("Script Executed .. "));
-  }
 });
